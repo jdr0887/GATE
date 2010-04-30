ab9387f645cfc20ce571f796f46cd55eb9a416fc
jdr0887 <jdr0887@gmail.com>
2010-04-30 14:02:18 -0400

    initial commit

---
+++ gate-renci-plugin/src/main/java/org/renci/gate/renci/Constants.java
@@ -0,0 +1,15 @@
+package org.renci.gate.renci;
+
+/**
+ * 
+ * @author jdr0887
+ */
+public interface Constants {
+
+    public final String CONFIG_FILE = "etc/org.renci.gate.site-selector-renci.cfg";
+
+//    public final String PROP_KEY_VO_NAME = "vo_name";
+//
+//    public final String PROP_KEY_CENTRAL_MASTER = "central_master";
+
+}
