b27b4dd8ba75dcc7f1fe6b36fc5021026f0f4174
Jason Reilly <jdr0887@renci.org>
2012-07-03 14:56:04 -0400

    initial commit

---
+++ gate-api/src/main/java/org/renci/gate/GlideinMetric.java
@@ -0,0 +1,55 @@
+package org.renci.gate;
+
+public class GlideinMetric {
+
+    private int running;
+
+    private int pending;
+
+    private String queue;
+
+    public GlideinMetric() {
+        super();
+    }
+
+    public GlideinMetric(int running, int pending, String queue) {
+        super();
+        this.running = running;
+        this.pending = pending;
+        this.queue = queue;
+    }
+
+    public String getQueue() {
+        return queue;
+    }
+
+    public void setQueue(String queue) {
+        this.queue = queue;
+    }
+
+    public int getRunning() {
+        return running;
+    }
+
+    public void setRunning(int running) {
+        this.running = running;
+    }
+
+    public int getTotal() {
+        return this.running + this.pending;
+    }
+
+    public int getPending() {
+        return pending;
+    }
+
+    public void setPending(int pending) {
+        this.pending = pending;
+    }
+
+    @Override
+    public String toString() {
+        return "GlideinMetrics [running=" + running + ", pending=" + pending + ", queue=" + queue + "]";
+    }
+
+}
---
+++ gate-api/src/main/java/org/renci/gate/SiteSnapshot.java
@@ -0,0 +1,9 @@
+package org.renci.gate;
+
+import java.util.List;
+
+public class SiteSnapshot {
+
+    private List<GlideinMetric> glideinMetricList;
+    
+}
