deb63627645893ad528c3b913b3c39f6296f854c
Jason Reilly <jdr0887@renci.org>
2012-07-09 11:02:04 -0400

    using SGEJobStatusInfo mapping instead of directly using SGEJobStatusType

--- gate-plugins/gate-plugin-lineberger/src/main/java/org/renci/gate/plugins/lineberger/LinebergerGATEService.java
+++ gate-plugins/gate-plugin-lineberger/src/main/java/org/renci/gate/plugins/lineberger/LinebergerGATEService.java
@@ -2,16 +2,20 @@ package org.renci.gate.plugins.lineberger;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.renci.gate.GATEService;
-import org.renci.gate.GlideinMetrics;
-import org.renci.gate.SiteInfo;
-import org.renci.jlrm.LRMException;
-import org.renci.jlrm.sge.SGEJobStatusType;
+import org.renci.gate.GlideinMetric;
+import org.renci.jlrm.JLRMException;
+import org.renci.jlrm.Queue;
+import org.renci.jlrm.Site;
+import org.renci.jlrm.sge.SGEJobStatusInfo;
 import org.renci.jlrm.sge.ssh.SGESSHFactory;
 import org.renci.jlrm.sge.ssh.SGESSHJob;
 import org.slf4j.Logger;
@@ -27,109 +31,122 @@ public class LinebergerGATEService implements GATEService {
 
     private final List<SGESSHJob> jobCache = new ArrayList<SGESSHJob>();
 
-    private SiteInfo siteInfo;
+    private Site site;
 
-    private String sgeHome;
+    private String collectorHost;
 
     public LinebergerGATEService() {
         super();
     }
 
-    public String getSGEHome() {
-        return sgeHome;
-    }
-
-    public void setSGEHome(String sgeHome) {
-        this.sgeHome = sgeHome;
-    }
-
-    public void setSiteInfo(SiteInfo siteInfo) {
-        this.siteInfo = siteInfo;
-    }
-
-    public SiteInfo getSiteInfo() {
-        return siteInfo;
-    }
-
     @Override
-    public GlideinMetrics lookupMetrics() {
-        GlideinMetrics metrics = new GlideinMetrics();
-        SGESSHFactory lsfSSHFactory = SGESSHFactory.getInstance(sgeHome, System.getProperty("user.name"),
-                siteInfo.getSubmitHost());
+    public Map<String, GlideinMetric> lookupMetrics() {
+        Map<String, GlideinMetric> metricsMap = new HashMap<String, GlideinMetric>();
+        SGESSHFactory lsfSSHFactory = SGESSHFactory.getInstance(site, System.getProperty("user.name"));
 
-        int running = 0;
-        int pending = 0;
         try {
 
-            Map<String, SGEJobStatusType> jobStatusMap = lsfSSHFactory.lookupStatus(jobCache
-                    .toArray(new SGESSHJob[jobCache.size()]));
+            Set<SGEJobStatusInfo> jobStatusSet = lsfSSHFactory.lookupStatus(jobCache.toArray(new SGESSHJob[jobCache
+                    .size()]));
+
+            // get unique list of queues
+            Set<String> queueSet = new HashSet<String>();
+            if (jobStatusSet != null) {
+                for (SGEJobStatusInfo info : jobStatusSet) {
+                    queueSet.add(info.getQueue());
+                }
+            }
 
             Iterator<SGESSHJob> jobCacheIter = jobCache.iterator();
             while (jobCacheIter.hasNext()) {
                 SGESSHJob job = jobCacheIter.next();
-                SGEJobStatusType status = jobStatusMap.get(job.getId());
-                switch (status) {
-                    case WAITING:
-                        ++pending;
-                        break;
-                    case RUNNING:
-                        ++running;
-                        break;
-                    case DELETION:
-                    case DONE:
-                        jobCacheIter.remove();
-                        break;
-                    case HOLD:
-                    case SUSPENDED:
-                    case TRANSFERING:
-                    case ERROR:
-                    case RESTARTED: 
-                    default:
-                        break;
+                for (String queue : queueSet) {
+                    int running = 0;
+                    int pending = 0;
+                    for (SGEJobStatusInfo info : jobStatusSet) {
+                        GlideinMetric metrics = new GlideinMetric();
+                        if (info.getQueue().equals(queue) && job.getId().equals(info.getJobId())) {
+                            switch (info.getType()) {
+                                case WAITING:
+                                    ++pending;
+                                    break;
+                                case RUNNING:
+                                    ++running;
+                                    break;
+                                case DELETION:
+                                case ERROR:
+                                case DONE:
+                                case THRESHOLD:
+                                    jobCacheIter.remove();
+                                    break;
+                                case SUSPENDED:
+                                case HOLD:
+                                case RESTARTED:
+                                case TRANSFERING:
+                                default:
+                                    break;
+                            }
+                        }
+                        metrics.setQueue(queue);
+                        metrics.setPending(pending);
+                        metrics.setRunning(running);
+                        metricsMap.put(queue, metrics);
+                    }
                 }
             }
-        } catch (LRMException e) {
+
+        } catch (JLRMException e) {
             e.printStackTrace();
             logger.error("Error:", e);
         }
-        metrics.setTotal(jobCache.size());
-        metrics.setPending(pending);
-        metrics.setRunning(running);
-        return metrics;
+        return metricsMap;
     }
 
     @Override
-    public void postGlidein() {
+    public void createGlidein(Queue queue) {
         File submitDir = new File("/tmp", System.getProperty("user.name"));
         submitDir.mkdirs();
         SGESSHJob job = null;
         try {
-            SGESSHFactory lsfSSHFactory = SGESSHFactory.getInstance(sgeHome, System.getProperty("user.name"),
-                    siteInfo.getSubmitHost());
-            job = lsfSSHFactory.submitGlidein(submitDir, siteInfo.getMaxNoClaimTime(), siteInfo.getMaxRunTime(), 40,
-                    siteInfo.getCondorCollectorHost(), siteInfo.getQueue());
+            SGESSHFactory lsfSSHFactory = SGESSHFactory.getInstance(this.site, System.getProperty("user.name"));
+            job = lsfSSHFactory.submitGlidein(submitDir, this.collectorHost, queue, 40);
             if (job != null && StringUtils.isNotEmpty(job.getId())) {
                 logger.info("job.getId(): {}", job.getId());
                 jobCache.add(job);
             }
-        } catch (LRMException e) {
+        } catch (JLRMException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void deleteGlidein() {
+    public void deleteGlidein(Queue queue) {
         if (jobCache.size() > 0) {
             try {
-                SGESSHFactory lsfSSHFactory = SGESSHFactory.getInstance(sgeHome, System.getProperty("user.name"),
-                        siteInfo.getSubmitHost());
+                SGESSHFactory lsfSSHFactory = SGESSHFactory.getInstance(this.site, System.getProperty("user.name"));
                 SGESSHJob job = jobCache.get(0);
                 lsfSSHFactory.killGlidein(job);
                 jobCache.remove(0);
-            } catch (LRMException e) {
+            } catch (JLRMException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public void setSite(Site site) {
+        this.site = site;
+    }
+
+    public String getCollectorHost() {
+        return collectorHost;
+    }
+
+    public void setCollectorHost(String collectorHost) {
+        this.collectorHost = collectorHost;
     }
 
 }
