c34925a10b2df270a30854cba3506a05e47293f5
Jason Reilly <jdr0887@renci.org>
2013-03-02 02:21:17 -0500

    not submitting sites with a score of 0

--- gate-engine/src/main/java/org/renci/gate/engine/SubmitGlideinRunnable.java
+++ gate-engine/src/main/java/org/renci/gate/engine/SubmitGlideinRunnable.java
@@ -154,9 +154,9 @@ public class SubmitGlideinRunnable implements Runnable {
                 winner = siteQueueScoreInfoList.get(random.nextInt(3));
             }
 
+            logger.info(winner.toString());
+            
             if (winner.getScore() > 0) {
-
-                logger.info(winner.toString());
 
                 GATEService gateService = gateServiceMap.get(winner.getSiteName());
                 Site siteInfo = gateService.getSite();
