ce916d3db700baaa662175009cf9a0a691f7b091
Jason Reilly <jdr0887@renci.org>
2013-09-05 12:55:19 -0400

    using strategy context

--- gate-engine/src/main/java/org/renci/gate/engine/GATEEngineRunnable.java
+++ gate-engine/src/main/java/org/renci/gate/engine/GATEEngineRunnable.java
@@ -1,25 +1,21 @@
 package org.renci.gate.engine;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
 import org.osgi.framework.ServiceReference;
 import org.renci.gate.GATEException;
 import org.renci.gate.GATEService;
 import org.renci.gate.GlideinMetric;
+import org.renci.gate.GlideinSubmissionBean;
+import org.renci.gate.GlideinSubmissionContext;
 import org.renci.gate.SiteQueueScore;
 import org.renci.jlrm.JLRMException;
-import org.renci.jlrm.Queue;
 import org.renci.jlrm.Site;
 import org.renci.jlrm.condor.ClassAdvertisement;
 import org.renci.jlrm.condor.ClassAdvertisementFactory;
@@ -31,8 +27,6 @@ import org.slf4j.LoggerFactory;
 public class GATEEngineRunnable implements Runnable {
 
     private final Logger logger = LoggerFactory.getLogger(GATEEngineRunnable.class);
-
-    private final Random random = new Random();
 
     private GATEServiceTracker serviceTracker;
 
@@ -72,12 +66,7 @@ public class GATEEngineRunnable implements Runnable {
             CondorLookupJobsByOwnerCallable callable = new CondorLookupJobsByOwnerCallable(username);
             jobMap.putAll(callable.call());
         } catch (JLRMException e) {
-            e.printStackTrace();
-        }
-
-        if (jobMap.size() == 0) {
-            logger.warn("no local condor jobs");
-            return;
+            logger.error("JLRMException", e);
         }
 
         int totalCondorJobs = jobMap.size();
@@ -92,9 +81,15 @@ public class GATEEngineRunnable implements Runnable {
         Map<String, Integer> requiredSiteMetricsMap = calculateRequiredSiteCount(jobMap);
 
         // get a snapshot of jobs across sites & queues
-        Map<String, Map<String, GlideinMetric>> siteQueueGlideinMetricsMap = globalMetricsLookup(gateServiceMap);
+        List<GlideinMetric> siteQueueGlideinMetricList = globalMetricsLookup(gateServiceMap);
 
-        boolean needGlidein = isGlideinNeeded(gateServiceMap, siteQueueGlideinMetricsMap, idleCondorJobs,
+        if (jobMap.size() == 0) {
+            Executors.newSingleThreadExecutor().execute(
+                    new KillGlideinRunnable(jobMap, gateServiceMap, siteQueueGlideinMetricList));
+            return;
+        }
+
+        boolean needGlidein = isGlideinNeeded(gateServiceMap, siteQueueGlideinMetricList, idleCondorJobs,
                 runningCondorJobs, totalCondorJobs);
 
         if (!needGlidein) {
@@ -102,73 +97,32 @@ public class GATEEngineRunnable implements Runnable {
             return;
         }
 
-        Map<String, Double> percentSiteRequiredJobOccuranceMap = new HashMap<String, Double>();
-        Double percentSiteRequiredJobOccuranceScore = 0.0;
-        for (String siteName : gateServiceMap.keySet()) {
-            logger.info("siteName = {}", siteName);
-            if (requiredSiteMetricsMap.containsKey(siteName)) {
-                percentSiteRequiredJobOccuranceScore = (double) (requiredSiteMetricsMap.get(siteName) / requiredSiteMetricsMap
-                        .size());
-                logger.info("percentSiteRequiredJobOccuranceScore = {}", percentSiteRequiredJobOccuranceScore);
-                percentSiteRequiredJobOccuranceMap.put(siteName, percentSiteRequiredJobOccuranceScore);
-            }
-        }
+        GlideinSubmissionBean glideinSubmissionBean = new GlideinSubmissionBean();
+        glideinSubmissionBean.setGateServiceMap(gateServiceMap);
+        glideinSubmissionBean.setIdleCondorJobs(idleCondorJobs);
+        glideinSubmissionBean.setRequiredSiteMetricsMap(requiredSiteMetricsMap);
+        glideinSubmissionBean.setRunningCondorJobs(runningCondorJobs);
+        glideinSubmissionBean.setSiteQueueGlideinMetricList(siteQueueGlideinMetricList);
 
-        List<SiteQueueScore> siteQueueScoreInfoList = new ArrayList<SiteQueueScore>();
+        GlideinSubmissionContext context = new GlideinSubmissionContext(glideinSubmissionBean);
+        List<SiteQueueScore> siteQueueScoreList = context.calculateSiteQueueScores();
 
-        for (String siteName : gateServiceMap.keySet()) {
-            GATEService gateService = gateServiceMap.get(siteName);
-            Site siteInfo = gateService.getSite();
-            Map<String, GlideinMetric> metricsMap = siteQueueGlideinMetricsMap.get(siteInfo.getName());
+        logger.info("siteQueueScoreList.size(): {}", siteQueueScoreList.size());
 
-            Double percentSiteRequiredJobOccurance = 0D;
-            if (percentSiteRequiredJobOccuranceMap.get(siteName) != null) {
-                percentSiteRequiredJobOccurance = percentSiteRequiredJobOccuranceMap.get(siteName);
-            }
-            LocalCondorMetric localCondorMetrics = new LocalCondorMetric(siteName, idleCondorJobs, runningCondorJobs,
-                    percentSiteRequiredJobOccurance);
-            logger.info(localCondorMetrics.toString());
+        for (SiteQueueScore siteQueueScore : siteQueueScoreList) {
 
-            if (localCondorMetrics.getSiteRequiredJobOccurance() == 1.0) {
-                siteQueueScoreInfoList.clear();
-                siteQueueScoreInfoList.addAll(calculate(gateService, siteInfo, metricsMap, localCondorMetrics));
-                break;
-            }
+            int numberToSubmit = context.calculateNumberToSubmit();
+            logger.info("numberToSubmit: {}", numberToSubmit);
 
-            siteQueueScoreInfoList.addAll(calculate(gateService, siteInfo, metricsMap, localCondorMetrics));
-        }
-
-        logger.info("siteQueueScoreInfoList.size(): {}", siteQueueScoreInfoList.size());
-        if (siteQueueScoreInfoList.size() > 0) {
-
-            // sort list based on comparator...descending score
-            Collections.sort(siteQueueScoreInfoList, new Comparator<SiteQueueScore>() {
-                @Override
-                public int compare(SiteQueueScore o1, SiteQueueScore o2) {
-                    return o2.getScore().compareTo(o1.getScore());
-                }
-            });
-
-            SiteQueueScore winner = null;
-            if (siteQueueScoreInfoList.size() <= 3) {
-                winner = siteQueueScoreInfoList.get(0);
-            } else {
-                winner = siteQueueScoreInfoList.get(random.nextInt(3));
-            }
-
-            logger.info(winner.toString());
-
-            if (winner.getScore() > 0) {
-                Executors.newSingleThreadExecutor().execute(new SubmitGlideinRunnable(gateServiceMap, winner));
+            for (int i = 0; i < numberToSubmit; ++i) {
+                GATEService gateService = gateServiceMap.get(siteQueueScore.getSiteName());
+                Site siteInfo = gateService.getSite();
+                logger.info(String.format("Submitting %d of %d glideins for %s to %s:%s", i + 1, numberToSubmit,
+                        siteInfo.getUsername(), siteQueueScore.getSiteName(), siteQueueScore.getQueueName()));
+                Executors.newSingleThreadExecutor().execute(new SubmitGlideinRunnable(gateService, siteQueueScore));
             }
 
         }
-
-        if (jobMap != null && jobMap.size() == 0) {
-            Executors.newSingleThreadExecutor().execute(
-                    new KillGlideinRunnable(jobMap, gateServiceMap, siteQueueGlideinMetricsMap));
-        }
-
     }
 
     private Map<String, Integer> calculateRequiredSiteCount(Map<String, List<ClassAdvertisement>> jobMap) {
@@ -214,15 +168,15 @@ public class GATEEngineRunnable implements Runnable {
         return condorJobs;
     }
 
-    private Map<String, Map<String, GlideinMetric>> globalMetricsLookup(Map<String, GATEService> gateServiceMap) {
+    private List<GlideinMetric> globalMetricsLookup(Map<String, GATEService> gateServiceMap) {
         logger.info("ENTERING globalMetricsLookup(Map<String, GATEService>)");
 
-        Map<String, Map<String, GlideinMetric>> siteQueueGlideinMetricsMap = new HashMap<String, Map<String, GlideinMetric>>();
+        List<GlideinMetric> siteQueueGlideinMetricList = new ArrayList<GlideinMetric>();
 
         for (String siteName : gateServiceMap.keySet()) {
 
             GATEService gateService = gateServiceMap.get(siteName);
-            Map<String, GlideinMetric> glideinMetricMap = null;
+            List<GlideinMetric> glideinMetricList = null;
 
             try {
                 if (!gateService.isValid()) {
@@ -240,13 +194,13 @@ public class GATEEngineRunnable implements Runnable {
             }
 
             try {
-                glideinMetricMap = gateService.lookupMetrics();
+                glideinMetricList = gateService.lookupMetrics();
             } catch (Exception e) {
                 logger.warn("There was a problem looking up metrics", e);
             }
 
-            if (glideinMetricMap == null) {
-                logger.warn("null glideinMetricMap: {}", siteName);
+            if (glideinMetricList == null) {
+                logger.warn("null glideinMetricList: {}", siteName);
                 continue;
             }
 
@@ -256,15 +210,15 @@ public class GATEEngineRunnable implements Runnable {
                 e.printStackTrace();
             }
 
-            siteQueueGlideinMetricsMap.put(gateService.getSite().getName(), glideinMetricMap);
+            siteQueueGlideinMetricList.addAll(glideinMetricList);
         }
-        return siteQueueGlideinMetricsMap;
+        return siteQueueGlideinMetricList;
     }
 
     private boolean isGlideinNeeded(Map<String, GATEService> gateServiceMap,
-            Map<String, Map<String, GlideinMetric>> siteQueueGlideinMetricsMap, int idleCondorJobs,
-            int runningCondorJobs, int totalCondorJobs) {
-        logger.info("ENTERING isGlideinNeeded(Map<String, GATEService>, Map<String, Map<String, GlideinMetric>>, int, int, int)");
+            List<GlideinMetric> siteQueueGlideinMetricList, int idleCondorJobs, int runningCondorJobs,
+            int totalCondorJobs) {
+        logger.info("ENTERING isGlideinNeeded(Map<String, GATEService>, List<GlideinMetric>, int, int, int)");
 
         // assume we need new glideins, and then run some tests to negate the assumptions
         boolean needGlidein = true;
@@ -282,19 +236,13 @@ public class GATEEngineRunnable implements Runnable {
                 maxAllowableJobs += siteInfo.getMaxTotalPending() + siteInfo.getMaxTotalRunning();
 
                 logger.info(siteInfo.toString());
-                Map<String, GlideinMetric> metricsMap = siteQueueGlideinMetricsMap.get(siteInfo.getName());
 
-                if (metricsMap == null) {
-                    logger.warn("metricsMap is null for {}", siteName);
-                    continue;
-                }
-
-                for (String queue : metricsMap.keySet()) {
-                    logger.debug("queue: {}", queue);
-                    GlideinMetric metrics = metricsMap.get(queue);
-                    logger.info(metrics.toString());
-                    totalRunningGlideinJobs += metrics.getRunning();
-                    totalPendingGlideinJobs += metrics.getPending();
+                for (GlideinMetric glideinMetric : siteQueueGlideinMetricList) {
+                    logger.info(glideinMetric.toString());
+                    if (siteName.equals(glideinMetric.getSiteName())) {
+                        totalRunningGlideinJobs += glideinMetric.getRunning();
+                        totalPendingGlideinJobs += glideinMetric.getPending();
+                    }
                 }
 
             }
@@ -330,131 +278,6 @@ public class GATEEngineRunnable implements Runnable {
             logger.error("Error", e1);
         }
         return needGlidein;
-    }
-
-    private List<SiteQueueScore> calculate(GATEService gateService, Site siteInfo,
-            Map<String, GlideinMetric> metricsMap, LocalCondorMetric localCondorMetrics) {
-        logger.info("ENTERING calculate(GATEService, Site, Map<String, GlideinMetric>, LocalCondorMetrics)");
-        List<SiteQueueScore> ret = new ArrayList<SiteQueueScore>();
-
-        Map<String, Queue> siteQueueInfoMap = siteInfo.getQueueInfoMap();
-
-        for (String queueName : siteQueueInfoMap.keySet()) {
-
-            Queue queueInfo = siteQueueInfoMap.get(queueName);
-
-            if (StringUtils.isNotEmpty(gateService.getActiveQueues())) {
-                List<String> activeQueueList = Arrays.asList(gateService.getActiveQueues().split(","));
-                if (!activeQueueList.contains(queueName)) {
-                    logger.info("excluding \"{}\" queue due to not being active", queueName);
-                    continue;
-                }
-            }
-
-            SiteQueueScore siteScoreInfo = new SiteQueueScore();
-            siteScoreInfo.setSiteName(siteInfo.getName());
-            siteScoreInfo.setQueueName(queueName);
-
-            GlideinMetric metrics = metricsMap.get(queueName);
-
-            Integer numberToSubmit = calculateNumberToSubmit(siteInfo, queueInfo, metrics,
-                    localCondorMetrics.getRunning(), localCondorMetrics.getIdle());
-            siteScoreInfo.setNumberToSubmit(numberToSubmit);
-
-            if (metrics.getTotal() == 0) {
-                siteScoreInfo.setMessage("No glideins have been submitted yet");
-                siteScoreInfo.setScore(200);
-                ret.add(siteScoreInfo);
-                continue;
-            }
-
-            logger.info(metrics.toString());
-
-            if (metrics.getPending() >= siteInfo.getMaxTotalPending()) {
-                logger.info("Pending job threshold has been met: {} of {}", metrics.getPending(),
-                        siteInfo.getMaxTotalPending());
-                siteScoreInfo.setMessage("No glideins needed...pending job threshold has been met");
-                siteScoreInfo.setScore(0);
-                ret.add(siteScoreInfo);
-                continue;
-            }
-
-            int totalJobs = metrics.getRunning() + metrics.getPending();
-
-            if (totalJobs == 0) {
-                // we want to start all sites at the same score so that we can give
-                // all sites a chance - this helps initial startup when a user submits
-                // one or a few jobs
-                siteScoreInfo.setMessage("Total number of glideins: " + totalJobs);
-                siteScoreInfo.setScore(200);
-                ret.add(siteScoreInfo);
-                continue;
-            }
-
-            if (totalJobs >= queueInfo.getMaxJobLimit()) {
-                // don't use this queue
-                siteScoreInfo.setMessage("Queue is maxed out");
-                siteScoreInfo.setScore(0);
-                ret.add(siteScoreInfo);
-                continue;
-            }
-
-            double score = 100;
-            double pendingWeight = 6.5;
-            for (int i = 1; i < metrics.getPending() + 1; ++i) {
-                score -= i * pendingWeight;
-            }
-            logger.info("penalized score = {}", score);
-            double runningWeight = 4.5;
-            for (int i = 1; i < metrics.getRunning() + 1; ++i) {
-                score += i * runningWeight;
-            }
-            logger.info("rewarded score = {}", score);
-            score *= queueInfo.getWeight();
-            logger.info("adjusted by queue weight = {}", score);
-            if (localCondorMetrics.getSiteRequiredJobOccurance() > 0) {
-                score += localCondorMetrics.getSiteRequiredJobOccurance() * 100;
-                logger.info("adjusted by siteRequiredJobOccurance = {}", score);
-            }
-
-            // when a lot of glideins are running, lower the score to spread the
-            // jobs out between the sites
-            if (score > 200) {
-                siteScoreInfo.setMessage("Score lowered to spread jobs out on available sites");
-                siteScoreInfo.setScore(200);
-                ret.add(siteScoreInfo);
-                continue;
-            }
-
-            siteScoreInfo.setMessage("Total number of glideins: " + metrics.getTotal());
-            siteScoreInfo.setScore(Long.valueOf(Math.round(score)).intValue());
-            logger.info(siteScoreInfo.toString());
-            ret.add(siteScoreInfo);
-
-        }
-
-        return ret;
-    }
-
-    private Integer calculateNumberToSubmit(Site siteInfo, Queue queueInfo, GlideinMetric metrics,
-            int runningCondorJobs, int idleCondorJobs) {
-        double numToSubmit = 1;
-        numToSubmit = queueInfo.getMaxMultipleJobsToSubmit();
-        if (metrics != null) {
-            numToSubmit -= metrics.getPending() * 0.4;
-            numToSubmit -= metrics.getRunning() * 0.4;
-        }
-        numToSubmit -= runningCondorJobs * 0.005;
-        numToSubmit += idleCondorJobs * 0.005;
-
-        if (numToSubmit <= 1 && (metrics != null && metrics.getRunning() <= siteInfo.getMaxTotalRunning())) {
-            numToSubmit = 1;
-        }
-
-        numToSubmit = Math.round(numToSubmit);
-        int ret = Double.valueOf(Math.min(numToSubmit, queueInfo.getMaxMultipleJobsToSubmit())).intValue();
-        // return ret;
-        return 1;
     }
 
 }
